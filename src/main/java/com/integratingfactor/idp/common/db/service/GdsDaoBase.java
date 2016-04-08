513f73cd7236a12a34b48ee593893c5ef37a396e
Amit Bhadoria <gnulib@gmail.com>
2016-04-08 02:56:09 -0700

    adding namespace env key

--- src/main/java/com/integratingfactor/idp/common/db/service/GdsDaoBase.java
+++ src/main/java/com/integratingfactor/idp/common/db/service/GdsDaoBase.java
@@ -2,6 +2,8 @@ package com.integratingfactor.idp.common.db.service;
 
 public interface GdsDaoBase {
 
+    static final String GdsDaoNameSpaceEnvKey = "idp.service.db.keyspace.name";
+
     String nameSpace();
 
     void cleanupExpired();
